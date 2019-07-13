4c5a47bb257dcb5504b639a642c1d87f5c7c9c91
WestleyK <westley@sylabs.io>
2019-07-12 20:18:10 -0700

    Improvments to code and functions

--- src/main-lf.c
+++ src/main-lf.c
@@ -1,7 +1,7 @@
 // created by: WestleyR
 // email: westleyr@nym.hush.com
 // https://github.com/WestleyR/list-files
-// date: Jul 6, 2019
+// date: Jul 13, 2019
 // version-1.0.0
 //
 // The Clear BSD License
@@ -30,9 +30,10 @@
 #define BOLDWHITE "\033[1m\033[37m"   // bold white
 #define COLORRESET "\033[0m"          // reset
 
-#define SCRIPT_VERSION "v1.0.0-beta-14, Jul 6, 2019"
+#define SCRIPT_VERSION "v1.0.0-beta-16, Jul 13, 2019"
 
 char *script_name;
+char *base_path = NULL;
 
 void help_menu() {
     printf("Usage:\n");
@@ -61,198 +62,174 @@ char* readable_fs(double size, char *buf) {
         i++;
     }
     sprintf(buf, "%.*f %s", i, size, units[i]);
-    return buf;
+    return(buf);
 }
 
-char* concat(const char *s1, const char *s2) {
+/*char* concat(const char *s1, const char *s2) {
     char *result = malloc(strlen(s1) + strlen(s2) + 1);
     // TODO: in real code you would check for errors in malloc here
     strcpy(result, s1);
     strcat(result, s2);
     return(result);
+}*/
+
+int file_info(const char* file_path) {
+    struct stat sb;
+    struct stat info;
+    char buf[10];
+    char full_file_path[256];
+    full_file_path[0] = '\0';
+    strcat(full_file_path, base_path);
+    strcat(full_file_path, "/");
+    strcat(full_file_path, file_path);
+        if (lstat(full_file_path, &info) != 0) {
+            perror("lstat");
+            printf("error: unable to open stat on: %s\n", full_file_path);
+            exit(20);
+        }
+
+        // TODO: use 'l' for links
+        printf((S_ISDIR(info.st_mode)) ? "d" : "-");
+        printf((info.st_mode & S_IRUSR) ? "r" : "-");
+        printf((info.st_mode & S_IWUSR) ? "w" : "-");
+        printf((info.st_mode & S_IXUSR) ? "x" : "-");
+        printf((info.st_mode & S_IRGRP) ? " r" : " -");
+        printf((info.st_mode & S_IWGRP) ? "w" : "-");
+        printf((info.st_mode & S_IXGRP) ? "x" : "-");
+        printf((info.st_mode & S_IROTH) ? "r" : "-");
+        printf((info.st_mode & S_IWOTH) ? "w" : "-");
+        printf((info.st_mode & S_IXOTH) ? "x" : "-");
+
+        struct passwd *pw = getpwuid(info.st_uid);
+        struct group *gr = getgrgid(info.st_gid);
+
+        printf("  %-8s : ", pw->pw_name);
+        printf("%-8s ", gr->gr_name);
+        printf(" %-12s ", readable_fs(info.st_size, buf));
+
+        if (S_ISLNK(info.st_mode)) {
+            char symlink_path[256];
+            ssize_t len = readlink(full_file_path, symlink_path, sizeof(symlink_path));
+            if (len != -1) {
+                symlink_path[len] = '\0';
+            } else {
+                printf("unable to fine link");
+            }
+            printf ("  %s%s  %s->  %s\n", BOLDCYAN, file_path, COLORRESET, symlink_path);
+        } else if (S_ISDIR(info.st_mode)) {
+            printf("  %s%s%s\n", BOLDBLUE, file_path, COLORRESET);
+        } else if (stat(full_file_path, &sb) == 0 && sb.st_mode & S_IXUSR) {
+            printf("  %s%s%s\n", BOLDGREEN, file_path, COLORRESET);
+        } else if (access(full_file_path, R_OK) != 0) {
+            printf("  %s%s%s\n", BOLDMAGENTA, file_path, COLORRESET);
+        } else {
+            printf("  %s\n", file_path);
+        }
+
+    return(0);
 }
 
-int main(int argc, char** argv) {
-    script_name = argv[0];
-
-    struct dirent *de;
+// Will list files in a directory
+int list_files(const char* list_path, int list_all, int mr_list) {
     DIR *dr;
-    struct stat sb;
-    struct stat s;
-    char* fileName;
+    struct dirent *de;
 
-    int listAll = 0;
-    int longList = 0;
-
-    fileName = "./";
-
-    for (int i=1; i < argc; i++) {
-        if (strcmp(argv[i], "-a") == 0) {
-            listAll = 1;
-        } else if (strcmp(argv[i], "-l") == 0) {
-            longList = 1;
-        } else if (strcmp(argv[i], "--help") == 0) {
-            help_menu();
-            return(0);
-        } else if (strcmp(argv[i], "--version") == 0) {
-            version_print();
-            return(0);
-        } else if ((strstr(argv[i], "--") != argv[i]) && (strstr(argv[i], "--") != argv[i])) {
-                fileName = argv[i];
-        }
-    }
-
-    if (stat(fileName, &s) != 0) {
-        printf("%s: unable to open: %s\n", script_name, fileName);
-        return(1);
-    }
-
-    if (S_ISREG(s.st_mode)) {
-        printf("error: %s: is a plane file.\n", fileName);
-        return(1);
-    }
-    
-
-    if (access(fileName, F_OK) != -1) {
-        if (access(fileName, R_OK) == -1) {
-            fprintf(stderr, "lf: %s: Not readable.\n", fileName);
-            exit(1);
-        }
-    } else {
-        fprintf(stderr, "lf: cannot access %s: No such file or directory\n", fileName);
-        exit(1);
-    }
-
-    dr = opendir(fileName);
+    dr = opendir(list_path);
     while ((de = readdir(dr)) != NULL) {
-        if ((*de->d_name != '.') && (strcmp(de->d_name, "..") != 0)) {
-
-            struct stat info;
-            char buf[10];
-            char *full_file_path;
-            full_file_path = concat(fileName, de->d_name);
-            if (lstat(full_file_path, &info) != 0) {
-                printf("error: unable to open stat on: %s\n", de->d_name);
-                exit(20);
+        if (list_all == 0) {
+            if ((*de->d_name == '.') || (strcmp(de->d_name, "..") == 0)) {
+                continue;
             }
+        }
 
-            // TODO: use 'l' for links
-            printf((S_ISDIR(info.st_mode)) ? "d" : "-");
-            printf((info.st_mode & S_IRUSR) ? "r" : "-");
-            printf((info.st_mode & S_IWUSR) ? "w" : "-");
-            printf((info.st_mode & S_IXUSR) ? "x" : "-");
-            printf((info.st_mode & S_IRGRP) ? " r" : " -");
-            printf((info.st_mode & S_IWGRP) ? "w" : "-");
-            printf((info.st_mode & S_IXGRP) ? "x" : "-");
-            printf((info.st_mode & S_IROTH) ? "r" : "-");
-            printf((info.st_mode & S_IWOTH) ? "w" : "-");
-            printf((info.st_mode & S_IXOTH) ? "x" : "-");
-
-/*            if (S_ISDIR(info.st_mode)) {
-                printf("   d");
-            } else if (S_ISLNK(info.st_mode)) {
-                printf("   l");
-            } else {
-                printf("   f");
-            }
-            printf((info.st_mode & S_IRUSR) ? "r" : "-");
-            printf((info.st_mode & S_IWUSR) ? "w" : "-");
-            printf((info.st_mode & S_IXUSR) ? "x  " : "-  ");
-*/
-
-            struct passwd *pw = getpwuid(info.st_uid);
-            struct group *gr = getgrgid(info.st_gid);
-
-            printf("  %-8s : ", pw->pw_name);
-            printf("%-8s ", gr->gr_name);
-            printf(" %-12s ", readable_fs(info.st_size, buf));
-
-           if (S_ISLNK(info.st_mode)) {
-               char symlink_path[256];
-               ssize_t len = readlink(full_file_path, symlink_path, sizeof(symlink_path));
-               if (len != -1) {
-                   symlink_path[len] = '\0';
-               } else {
-                   printf("unable to fine link");
-               }
-               printf ("  %s%s  %s->  %s\n", BOLDCYAN, de->d_name, COLORRESET, symlink_path);
-            } else if (S_ISDIR(info.st_mode)) {
-                printf("  %s%s%s\n", BOLDBLUE, de->d_name, COLORRESET);
-            } else if (stat(full_file_path, &sb) == 0 && sb.st_mode & S_IXUSR) {
-                printf("  %s%s%s\n", BOLDGREEN, de->d_name, COLORRESET);
-            } else if (access(full_file_path, R_OK) != 0) {
-                printf("  %s%s%s\n", BOLDMAGENTA, de->d_name, COLORRESET);
-            } else {
-                printf("  %s\n", de->d_name);
-            }
-        } else if (listAll == 1) {
-            struct stat info;
-            char buf[10];
-            char *full_file_path;
-            full_file_path = concat(fileName, de->d_name);
-            if (lstat(full_file_path, &info) != 0) {
-                printf("error: unable to open stat on: %s\n", de->d_name);
-                exit(20);
-            }
-
-            // TODO: use 'l' for links
-            printf((S_ISDIR(info.st_mode)) ? "d" : "-");
-            printf((info.st_mode & S_IRUSR) ? "r" : "-");
-            printf((info.st_mode & S_IWUSR) ? "w" : "-");
-            printf((info.st_mode & S_IXUSR) ? "x" : "-");
-            printf((info.st_mode & S_IRGRP) ? "r" : "-");
-            printf((info.st_mode & S_IWGRP) ? "w" : "-");
-            printf((info.st_mode & S_IXGRP) ? "x" : "-");
-            printf((info.st_mode & S_IROTH) ? "r" : "-");
-            printf((info.st_mode & S_IWOTH) ? "w" : "-");
-            printf((info.st_mode & S_IXOTH) ? "x" : "-");
-
-            if (S_ISDIR(info.st_mode)) {
-                printf("   d");
-            } else if (S_ISLNK(info.st_mode)) {
-                printf("   l");
-            } else {
-                printf("   f");
-            }
-            printf((info.st_mode & S_IRUSR) ? "r" : "-");
-            printf((info.st_mode & S_IWUSR) ? "w" : "-");
-            printf((info.st_mode & S_IXUSR) ? "x  " : "-  ");
-
-            struct passwd *pw = getpwuid(info.st_uid);
-            struct group *gr = getgrgid(info.st_gid);
-
-            printf("  %-8s : ", pw->pw_name);
-            printf("%-8s ", gr->gr_name);
-
-            printf(" %-10s ", readable_fs(info.st_size, buf));
-
-           if (S_ISLNK(info.st_mode)) {
-               char symlink_path[256];
-               ssize_t len = readlink(full_file_path, symlink_path, sizeof(symlink_path));
-               if (len != -1) {
-                   symlink_path[len] = '\0';
-               } else {
-                   printf("unable to fine link");
-               }
-               printf ("  %s%s  %s->  %s\n", BOLDCYAN, de->d_name, COLORRESET, symlink_path);
-            } else if (S_ISDIR(info.st_mode)) {
-                printf("  %s%s%s\n", BOLDBLUE, de->d_name, COLORRESET);
-            } else if (stat(full_file_path, &sb) == 0 && sb.st_mode & S_IXUSR) {
-                printf("  %s%s%s\n", BOLDGREEN, de->d_name, COLORRESET);
-            } else if (access(full_file_path, R_OK) != 0) {
-                printf("  %s%s%s\n", BOLDMAGENTA, de->d_name, COLORRESET);
-            } else {
-                printf("  %s\n", de->d_name);
+        if (mr_list != 0) {
+            printf("%s\n", de->d_name);
+        } else {
+            if (file_info(de->d_name) != 0) {
+                printf("ERROR: while listing file: %s\n", de->d_name);
             }
         }
     }
     closedir(dr);
 
-    if (longList != 0) {
-        printf("\n");
+    return(0);
+}
+
+int prep_list(const char *file_path, int list_all, int mr_list) {
+    struct stat s;
+
+    if (stat(file_path, &s) != 0) {
+        printf("%s: unable to open: %s\n", script_name, file_path);
+        return(1);
     }
 
-    return 0;
+    if (S_ISREG(s.st_mode)) {
+        if (mr_list != 0) {
+            printf("%s\n", file_path);
+        }
+        if (file_info(file_path) != 0) {
+            printf("error while listing file\n");
+        }
+        return(0);
+    }
+    base_path = strdup(file_path);
+
+    if (access(file_path, F_OK) != -1) {
+        if (access(file_path, R_OK) == -1) {
+            fprintf(stderr, "lf: %s: Not readable.\n", file_path);
+            exit(1);
+        }
+    } else {
+        fprintf(stderr, "lf: cannot access %s: No such file or directory\n", file_path);
+        exit(1);
+    }
+
+    if (list_files(file_path, list_all, mr_list) != 0) {
+        printf("There was a problome listing files\n");
+        exit(1);
+    }
+
+    return(0);
+}
+
+int main(int argc, char** argv) {
+    script_name = argv[0];
+
+    char* file_path;
+    int arg_list = 0;
+
+    // -a option
+    int list_all = 0;
+
+    // mechine readable output
+    int mr_list = 0;
+
+    file_path = "./";
+    base_path = "";
+
+    for (int i=1; i < argc; i++) {
+        if (strcmp(argv[i], "-a") == 0) {
+            list_all = 1;
+        } else if ((strcmp(argv[i], "-1") == 0) || (strcmp(argv[i], "-m") == 0)) {
+            mr_list = 1;
+        } else if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0)) {
+            help_menu();
+            return(0);
+        } else if ((strcmp(argv[i], "-v") == 0) || (strcmp(argv[i], "--version") == 0)) {
+            version_print();
+            return(0);
+        } else if ((strstr(argv[i], "--") != argv[i]) && (strstr(argv[i], "--") != argv[i])) {
+            file_path = argv[i];
+            prep_list(file_path, list_all, mr_list);
+            arg_list = 1;
+        }
+    }
+
+    if (arg_list == 0) {
+        prep_list(file_path, list_all, mr_list);
+    }
+
+    return(0);
 }
 
 //
